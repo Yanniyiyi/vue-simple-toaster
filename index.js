3195c66cb9479aacebea0ace4c394ec9c9b026ad
Yi <yi@andatech.com.au>
2017-06-17 00:34:36 +1000

    add 3 animations

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ var Toaster = {};
 Toaster.install = function(Vue,options)
 {
 	let defaultOptions = {
+		type: 'normal',
 		position: 'top-left',
-		duration: 2500
+		duration: 2500,
+		animation:'fadeIn'
 	}
+
+	let validAnimations = [
+		'fadeIn','slideDown','slideUp'
+	]
 
 	let validPostions = [
 		'top','top-left','top-center','top-right',
@@ -21,18 +27,29 @@ Toaster.install = function(Vue,options)
 
 
 	if(validPostions.indexOf(defaultOptions.position) < 0){
-		defaultOptions.position = 'top-left';
+		defaultOptions.position = 'fadeIn';
 	};
 
-	Vue.prototype.$toaster = (message,position) => {
+	if(validAnimations.indexOf(defaultOptions.animation) < 0){
+		defaultOptions.animation = 'fadeIn';
+	};
+
+	Vue.prototype.$toaster = (message,position,animation) => {
+		let currentPosition = defaultOptions.position;
+		let currentAnimation = defaultOptions.animation;
 
 		if(position && validPostions.indexOf(position) >= 0)
 		{
-			defaultOptions.position = position;
+			currentPosition = position;
+		}
+
+		if(animation && validAnimations.indexOf(animation) >= 0)
+		{
+			currentAnimation = animation;
 		}
 
 		let toaster = Vue.extend({
-			template: '<div class="vue-simple-toaster toaster-' + defaultOptions.position + '">' + message + '</div>'
+			template: '<div class="vue-simple-toaster ' +  currentAnimation + ' toaster-' + currentPosition + '">' + message + '</div>'
 		});
 
 		let tempToaster = new toaster().$mount().$el;
